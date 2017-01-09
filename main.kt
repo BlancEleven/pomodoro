adf2a1d66cce14cf421e90ab438be8337c3432fe
Andrew B <mail@andrewblanchette.com>
2017-01-08 22:02:20 -0500

    Has printTime and formatUnit methods.

--- main.kt
+++ main.kt
@@ -6,10 +6,21 @@ class count(hh: Int, mm: Int, ss: Int){
     val frt = fixedRateTimer('tomato-time', false, 1000, 1000){
         (if ss == 59){
             ss = 0
-            if(hh == 59){
-                
+            if(mm < 59){
+                mm++
+            }else{
+                mm = 0
+                hh++
             }
         }
+    }
+
+    private fun printTime(){
+        //TODO This method prints the time in a string format.
+    }
+
+    private fun formatUnit(unit: Int): String{
+        //TODO This method formats the unit by adding a 0 to Int < 10.
     }
 }
 
